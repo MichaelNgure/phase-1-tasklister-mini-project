0170df40c6f804f1656771cfa706a869ce9927d8
MichaelNgure <mikynjoro01@gmail.com>
2024-07-05 15:08:38 +0300

    mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskDescriptionInput = document.getElementById('new-task-description')
+  const tasksList = document.getElementById('tasks')
+  const submitInput = document.getElementById('submitInput')
+  const newTaskForm = document.getElementById("create-task-form");
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  submitInput.addEventListener('click', task)
+
+  function task (){
+
+    const li = document.createElement('li')
+    li.textContent = newTaskDescriptionInput.value 
+    tasksList.append(li)
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'x'
+    li.appendChild(deleteButton)
+    deleteButton.addEventListener('click', deleteItem)
+    function deleteItem(){
+      li.remove()
+    }
+  }
+
 });
